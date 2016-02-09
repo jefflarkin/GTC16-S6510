b88f6cf68873a667e7d056e38927756cc2aa8e86
Jeff Larkin <jlarkin@nvidia.com>
2016-02-09 10:33:04 -0500

    Initial check-in of original and threaded code in C and Fortran.

---
+++ common.mk
@@ -0,0 +1,22 @@
+ifneq (,$(findstring gcc,$(OPENMP_CC)))
+OPENMP_CFLAGS=-O3 -fopenmp
+else
+OPENMP_CC=clang
+OPENMP_CFLAGS=-fopenmp=libomp -omptargets=nvptx64sm_35-nvidia-linux -Wno-unknown-pragmas
+endif
+ifneq (,$(findstring g++,$(OPENMP_CXX)))
+OPENMP_CXXFLAGS=-O3 -fopenmp
+else
+OPENMP_CXX=clang++
+OPENMP_CXXFLAGS=-I/usr/include/c++/4.8 -I/usr/include/x86_64-linux-gnu/c++/4.8 -I$(CLANG_HOME)/include -fopenmp=libomp -omptargets=nvptx64sm_35-nvidia-linux -Wno-unknown-pragmas
+endif
+ifneq (,$(findstring gfortran,$(OPENMP_FC)))
+OPENMP_FCFLAGS=-O3 -fopenmp
+else
+# CLANG currently does not support Fortran
+OPENMP_FC=clang++
+OPENMP_FCFLAGS=-I/usr/include/c++/4.8 -I/usr/include/x86_64-linux-gnu/c++/4.8 -I$(CLANG_HOME)/include -fopenmp=libomp -omptargets=nvptx64sm_35-nvidia-linux -Wno-unknown-pragmas
+endif
+
+OPENMP_LDFLAGS += -lm -L/usr/local/cuda/nvvm/libdevice
+
